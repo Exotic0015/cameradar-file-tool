775bb92188ec2c7b595c2963a4cc94cf6e156c78
Exotic <jalokim541515@gmail.com>
2019-08-14 12:01:40 +0200

    Fixes

--- cmrdr.sh
+++ cmrdr.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
-LOCATION=$1
-TARGETS="`cat $LOCATION | tr '\n' ','`"
-$GOPATH/bin/cameradar -t ${TARGETS::-1}
+# Written by Exotic#1124
+echo 'If there is an error or nothing happens, make sure you specified a valid file and GOPATH is set.'
+TARGETS="`cat $1`"
+IFS=$'\n'
+set -f
+for i in $TARGETS; do
+    echo 'Attacking '$i'...'
+    $GOPATH/bin/cameradar run -t $i
+done
