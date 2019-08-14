90d14184e8c85a0bc10bce3f23687d67929ffe07
Exotic <jalokim541515@gmail.com>
2019-08-14 12:42:59 +0200

    Added an ability to generate output

--- cmrdr.sh
+++ cmrdr.sh
@@ -2,9 +2,17 @@
 # Written by Exotic#1124
 echo 'If there is an error or nothing happens, make sure you specified a valid file and GOPATH is set.'
 TARGETS="`cat $1`"
+OUTPUT=$2
 IFS=$'\n'
 set -f
-for i in $TARGETS; do
-    echo 'Attacking '$i'...'
-    $GOPATH/bin/cameradar run -t $i
-done
+if [[ $OUTPUT != "" ]]; then
+    for i in $TARGETS; do
+        echo 'Attacking '$i'...'
+       $GOPATH/bin/cameradar run -t $i |& tee $OUTPUT
+    done
+else
+    for i in $TARGETS; do
+        echo 'Attacking '$i'...'
+       $GOPATH/bin/cameradar run -t $i
+    done
+fi
