76780741621237833ef32e12c7e68472280d223b
Exotic0015 <jalokim541515@gmail.com>
2019-08-17 16:52:49 +0200

    Rewritten half of the script

--- cmrdr.sh
+++ cmrdr.sh
@@ -3,21 +3,40 @@
 echo 'If there is an error or nothing happens, make sure you specified a valid file and GOPATH is set.'
 exitfn () {
     trap SIGINT
-    echo -e '\nExiting...'
-    awk "!/  > Perform failed: curl: Failure when receiving data from the peer/" $OUTPUT > temp && mv temp $OUTPUT
-    awk "!/  > Perform failed: curl: Couldn't connect to server/" $OUTPUT > temp && mv temp $OUTPUT
-    awk "!/  > Perform failed: curl: Timeout was reached/" $OUTPUT > temp && mv temp $OUTPUT
-    exit
+    echo -e '\nInterrupt detected!\nExiting...'
+    if [[ $OUTPUT != "" ]]; then
+        awk "!/  > Perform failed: curl: Failure when receiving data from the peer/" $OUTPUT > temp && mv temp $OUTPUT
+        awk "!/  > Perform failed: curl: Couldn't connect to server/" $OUTPUT > temp && mv temp $OUTPUT
+        awk "!/  > Perform failed: curl: Timeout was reached/" $OUTPUT > temp && mv temp $OUTPUT
+    fi
+    exit 1
 }
 trap "exitfn" INT
-TARGETS="`cat $1`"
-OUTPUT=$2
+# Variables
+OPTIND=1
+OUTPUT=""
 IFS=$'\n'
 set -f
+# Code
+while getopts ":f:o:" argument; do
+    case "${argument}" in
+        f)
+            TARGETS="`cat ${OPTARG}`"
+            ;;
+        o)
+            OUTPUT=${OPTARG}
+            ;;
+        ?)
+            echo "Usage: $0 [-f iplist_filename] [-o output_filename]" >&2
+            exit 1
+            ;;
+    esac
+done
+shift $((OPTIND-1))
 if [[ $OUTPUT != "" ]]; then
     echo -n "" > $OUTPUT
     for i in $TARGETS; do
-        echo 'Attacking '$i'...'
+       echo 'Attacking '$i'...'
        $GOPATH/bin/cameradar run -t $i |& tee -a $OUTPUT
        awk "!/  > Perform failed: curl: Failure when receiving data from the peer/" $OUTPUT > temp && mv temp $OUTPUT
        awk "!/  > Perform failed: curl: Couldn't connect to server/" $OUTPUT > temp && mv temp $OUTPUT
@@ -29,4 +48,5 @@ else
        $GOPATH/bin/cameradar run -t $i
     done
 fi
+# End
 trap SIGINT
